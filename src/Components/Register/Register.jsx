593526573e66c99bb45289941b01a19523fa4471
Manuel.Millan1979 <nutriglar@gmail.com>
2024-10-05 20:07:55 -0300

    cambios register

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,74 +1,6 @@
-import { useState } from 'react';
-import backgroundImage from '../../../public/bg.jpeg';
-
-const styles = {
-  pageContainer: {
-    backgroundImage: `url(${backgroundImage})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    height: '100vh',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  formContainer: {
-    backgroundColor: 'rgba(255, 255, 255, 0.8)',
-    padding: '20px',
-    borderRadius: '8px',
-    boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-    width: '100%',
-    maxWidth: '500px',
-    boxSizing: 'border-box',
-    position: 'relative',
-  },
-  h1: {
-    textAlign: 'center',
-    color: '#333',
-  },
-  form: {
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  input: {
-    padding: '10px',
-    marginBottom: '15px',
-    border: '1px solid #ccc',
-    borderRadius: '4px',
-    fontSize: '1rem',
-    width: '100%',
-    boxSizing: 'border-box',
-    backgroundColor: '#CA27C9',
-    color: '#fff',
-  },
-  buttonContainer: {
-    display: 'flex',
-    justifyContent: 'space-between',
-  },
-  button: {
-    padding: '10px',
-    backgroundColor: '#06B4CF',
-    color: '#fff',
-    border: 'none',
-    borderRadius: '4px',
-    cursor: 'pointer',
-    flexGrow: 1,
-    fontSize: '1rem',
-    margin: '5px',
-    fontWeight: 'bold',
-  },
-  buttonPrev: {
-    padding: '10px',
-    backgroundColor: '#FFEB3B',
-    color: '#333',
-    border: 'none',
-    borderRadius: '4px',
-    cursor: 'pointer',
-    flexGrow: 1,
-    fontSize: '1rem',
-    margin: '5px',
-    fontWeight: 'bold',
-  },
-};
+import React, { useState } from 'react';
+import './register.css'; // Importamos los estilos desde el archivo CSS
+import backgroundImage from '../../../public/bg.jpeg'; // Importamos la imagen de fondo
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -107,15 +39,15 @@ const Register = () => {
   };
 
   return (
-    <div style={styles.pageContainer}>
-      <div style={styles.formContainer}>
-        <h1 style={styles.h1}>Register in our app and don´t let any event slip through your mind</h1>
-        <form style={styles.form} onSubmit={handleSubmit}>
+    <div className="page-container" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="form-container">
+        <h1 className="form-title">Register in our app and don’t let any event slip through your mind</h1>
+        <form className="form" onSubmit={handleSubmit}>
           {/* Página 1 */}
           {step === 1 && (
             <>
               <input
-                style={styles.input}
+                className="input"
                 type="text"
                 name="name"
                 value={formData.name}
@@ -123,7 +55,7 @@ const Register = () => {
                 placeholder="First Name"
               />
               <input
-                style={styles.input}
+                className="input"
                 type="text"
                 name="lastName"
                 value={formData.lastName}
@@ -131,7 +63,7 @@ const Register = () => {
                 placeholder="Last Name"
               />
               <input
-                style={styles.input}
+                className="input"
                 type="email"
                 name="email"
                 value={formData.email}
@@ -139,7 +71,7 @@ const Register = () => {
                 placeholder="Email"
               />
               <input
-                style={styles.input}
+                className="input"
                 type="password"
                 name="password"
                 value={formData.password}
@@ -147,14 +79,14 @@ const Register = () => {
                 placeholder="Password"
               />
               <input
-                style={styles.input}
+                className="input"
                 type="password"
                 name="repeatPassword"
                 value={formData.repeatPassword}
                 onChange={handleChange}
                 placeholder="Repeat Password"
               />
-              <button type="button" style={styles.button} onClick={handleNextStep}>
+              <button type="button" className="button" onClick={handleNextStep}>
                 Continue
               </button>
             </>
@@ -164,7 +96,7 @@ const Register = () => {
           {step === 2 && (
             <>
               <input
-                style={styles.input}
+                className="input"
                 type="date"
                 name="birthDate"
                 value={formData.birthDate}
@@ -172,7 +104,7 @@ const Register = () => {
                 placeholder="Birth Date"
               />
               <input
-                style={styles.input}
+                className="input"
                 type="text"
                 name="address"
                 value={formData.address}
@@ -180,7 +112,7 @@ const Register = () => {
                 placeholder="Address"
               />
               <input
-                style={styles.input}
+                className="input"
                 type="text"
                 name="region"
                 value={formData.region}
@@ -188,26 +120,26 @@ const Register = () => {
                 placeholder="Region"
               />
               <input
-                style={styles.input}
+                className="input"
                 type="text"
                 name="commune"
                 value={formData.commune}
                 onChange={handleChange}
                 placeholder="Commune"
               />
-              <div style={styles.buttonContainer}>
-                <button type="button" style={styles.buttonPrev} onClick={handlePrevStep}>
+              <div className="button-container">
+                <button type="button" className="button-prev" onClick={handlePrevStep}>
                   Back
                 </button>
-                <button type="submit" style={styles.button}>
+                <button type="submit" className="button">
                   Register
                 </button>
               </div>
             </>
           )}
         </form>
-        <p style={styles.p}>
-        Cant remember your password? <a href="#" style={styles.a} onMouseOver={(e) => (e.target.style.textDecoration = styles.aHover.textDecoration)} onMouseOut={(e) => (e.target.style.textDecoration = styles.a.textDecoration)}>Click here</a>
+        <p className="forgot-password">
+          Can't remember your password? <a href="#" className="link">Click here</a>
         </p>
       </div>
     </div>
