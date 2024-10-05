66c67eb94728e0044c0b63cd04d6d46b2c8c919a
Manuel.Millan1979 <nutriglar@gmail.com>
2024-10-05 20:00:16 -0300

    cambios login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,94 +1,38 @@
+import React from 'react';
+import './Login.css';
 import backgroundImage from '../../../public/bg.jpeg';
-
-
-const styles = {
-  pageContainer: {
-    backgroundImage: `url(${backgroundImage})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    height: '100vh',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  loginContainer: {
-    backgroundColor: 'rgba(255, 255, 255, 0.8)',
-    padding: '20px',
-    borderRadius: '8px',
-    boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-    width: '100%',
-    maxWidth: '500px',
-    boxSizing: 'border-box',
-    textAlign: 'center',
-  },
-  h1: {
-    fontSize: '1.5em',
-    marginBottom: '20px',
-    color: '#333',
-  },
-  form: {
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  label: {
-    marginBottom: '5px',
-    color: '#333',
-  },
-  input: {
-    padding: '8px',
-    marginBottom: '15px',
-    border: '1px solid #ccc',
-    borderRadius: '4px',
-  },
-  button: {
-    padding: '10px',
-    backgroundColor: '#ff00ff',
-    color: '#fff',
-    border: 'none',
-    borderRadius: '4px',
-    cursor: 'pointer',
-  },
-  buttonHover: {
-    backgroundColor: '#cc00cc',
-  },
-  p: {
-    marginTop: '20px',
-    color: '#333',
-  },
-  a: {
-    color: '#ff00ff',
-    textDecoration: 'none',
-  },
-  aHover: {
-    textDecoration: 'underline',
-  },
-};
 
 const Login = () => {
   return (
-    <div style={styles.pageContainer}>
-      <div style={styles.loginContainer}>
-        <h1 style={styles.h1}>Register your events and let Aidie manage your reminders!</h1>
-        <form style={styles.form}>
-          <label style={styles.label}>
-            Email:
-            <input style={styles.input} type="email" name="email" />
+    <div className="page-container" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="login-container">
+        <h1 className="login-title">Register your events and let Aidie manage your reminders!</h1>
+        <form className="form">
+          <label className="label">
+            <input
+              className="input"
+              type="email"
+              name="email"
+              placeholder="Email"
+            />
           </label>
-          <label style={styles.label}>
-            Password:
-            <input style={styles.input} type="password" name="password" />
-        </label>
-        <button
+          <label className="label">
+            <input
+              className="input"
+              type="password"
+              name="password"
+              placeholder="Password"
+            />
+          </label>
+          <button
             type="submit"
-            style={styles.button}
-            onMouseOver={(e) => (e.target.style.backgroundColor = styles.buttonHover.backgroundColor)}
-            onMouseOut={(e) => (e.target.style.backgroundColor = styles.button.backgroundColor)}
-        >
-            LETS GO!
-        </button>
+            className="button"
+          >
+            LET'S GO!
+          </button>
         </form>
-        <p style={styles.p}>
-        Cant remember your password? <a href="#" style={styles.a} onMouseOver={(e) => (e.target.style.textDecoration = styles.aHover.textDecoration)} onMouseOut={(e) => (e.target.style.textDecoration = styles.a.textDecoration)}>Click here</a>
+        <p className="paragraph">
+          Can't remember your password? <a href="#" className="link">Click here</a>
         </p>
       </div>
     </div>
