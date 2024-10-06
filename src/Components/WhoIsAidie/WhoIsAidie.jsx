0f9b31cb95a5f925324631485e35fd7fa5fd0b50
Manuel.Millan1979 <nutriglar@gmail.com>
2024-10-06 13:14:49 -0300

    cambio

--- src/Components/WhoIsAidie/WhoIsAidie.jsx
+++ src/Components/WhoIsAidie/WhoIsAidie.jsx
@@ -1,15 +1,15 @@
 import './WhoIsAidie.css';
 import aidieLarge from "../../../public/imag2.png";
 import aidieSmall from "../../../public/imag1.png";
-import maps from "../../../public/maps.png";
+import maps from "../../../public/maps.png"; // Uso correcto de la imagen
 
 const WhoIsAidie = () => {
   return (
     <div className="presentation-container">
       <div className="top-section">
         <div className="text-box pink-border">
-        <h1>Meet Aidie, your event assistant</h1>
-        <p>Aidie will help you to not miss your events</p>
+          <h1>Meet Aidie, your event assistant</h1>
+          <p>Aidie will help you to not miss your events</p>
         </div>
         <div className="character-image">
           <img src={aidieLarge} alt="Aidie Large" className="aidie-large" />
@@ -17,21 +17,22 @@ const WhoIsAidie = () => {
       </div>
 
       <div className="middle-section">
-        <div className="text-box">
+        <div className="text-box middle-20">
           <img src={aidieSmall} alt="Aidie Small" className="aidie-small" />
-		  <h2>Pick your favorite artist</h2>
+          <h2>Pick your favorite artist</h2>
         </div>
-        <div className="text-box">
-          <h3>Aidie will provide the next events in <br /> your area based on your preferents <img src="maps.png" alt="Icon" className="inline-image" /></h3>
+        <div className="text-box middle-50">
+          <h3>Aidie will provide <br /> the next events in <br /> your area based on <br /> your preferences <br />
+            <img
+              src={maps}
+              alt="Map Icon"
+              className="inline-image"
+              style={{ width: "40px", height: "40px", marginLeft: "5px" }} // Tamaño ajustado
+            />
+          </h3>
         </div>
-      </div>
-
-      {/* Parte inferior: 2 divs uno al lado del otro */}
-      <div className="bottom-section">
-        <div className="text-box">
+        <div className="text-box middle-50">
           <p>Choose your preferences and Aidie will handle the rest</p>
-        </div>
-        <div className="text-box">
           <p>Get reminders for events, updates, and more</p>
         </div>
       </div>
