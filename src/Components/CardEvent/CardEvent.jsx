55cfc279c77a707b3ac1ab8a84493d26fa00b188
Shirley Mallea <85321043+shimapi@users.noreply.github.com>
2024-10-05 21:46:14 -0300

    feat: working on event

--- src/Components/CardEvent/CardEvent.jsx
+++ src/Components/CardEvent/CardEvent.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./CardEvent.css";
+import { AppContext } from "../Contexto/AppContext";
+import { FaRegStar } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
+import { useState } from "react";
 
 const event = {
 	id: "1",
@@ -14,13 +18,16 @@ const event = {
 		"Live concert featuring an energetic performance by The Rockers.",
 	hour: "20:00 hrs",
 	saleStatus: 1,
-	userFavorite: false,
+	userFavorite: true,
 };
+
+const [isFavorite, setIsFavorite] = useState(event.userFavorite);
 
 const handleFavoriteChange = () => {
 	console.log("Favorite changed");
 	event.userFavorite = !event.userFavorite;
 	console.log(event.userFavorite);
+	setIsFavorite(event.userFavorite);
 };
 
 const CardEvent = () => {
@@ -35,38 +42,38 @@ const CardEvent = () => {
 					checked={event.userFavorite}
 					onChange={handleFavoriteChange}
 				/>
-				<label htmlFor="favorite">Favorite</label>
+				{event.userFavorite ? (
+					<FaStar className="star-icon" htmlFor="favorite" />
+				) : (
+					<FaRegStar className="star-icon" htmlFor="favorite" />
+				)}
 			</div>
-			<h3 className="event-name">{event.name}</h3>
-			<p className="event-description">{event.description}</p>
-			<p className="event-date">
-				<span>When?</span>
-				{event.date}
-			</p>
-			<p className="event-location">
-				<span>Where?</span>
-				{event.location}
-			</p>
-			<p className="event-location">
-				<span>What time?</span>
-				{event.hour}
-			</p>
+			<div>
+				<h3 className="event-name">{event.name}</h3>
+				<p className="event-description">{event.description}</p>
+				<p className="event-date">
+					<span>When?</span>
+					{event.date}
+				</p>
+				<p className="event-location">
+					<span>Where?</span>
+					{event.location}
+				</p>
+				<p className="event-location">
+					<span>What time?</span>
+					{event.hour}
+				</p>
 
-			{event.saleStatus === 1 && (
-				<>
-					{console.log(1)}
+				{event.saleStatus === 1 && (
 					<button className="buy-button">Buy tickets!</button>
-				</>
-			)}
+				)}
 
-			{event.saleStatus === 2 && (
-				<>
-					{console.log(2)}
+				{event.saleStatus === 2 && (
 					<button className="tellme-button">
 						Let me know when the sale starts!
 					</button>
-				</>
-			)}
+				)}
+			</div>
 		</article>
 	);
 };
