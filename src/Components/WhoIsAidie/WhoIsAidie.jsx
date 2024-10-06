76a54762b1097d710c9c79eb850f0bacb6a804d0
Manuel.Millan1979 <nutriglar@gmail.com>
2024-10-06 12:39:04 -0300

    add who is aidie

--- src/Components/WhoIsAidie/WhoIsAidie.jsx
+++ src/Components/WhoIsAidie/WhoIsAidie.jsx
@@ -1,5 +1,42 @@
+import './WhoIsAidie.css';
+import aidieLarge from "../../../public/imag2.png";
+import aidieSmall from "../../../public/imag1.png";
+import maps from "../../../public/maps.png";
+
 const WhoIsAidie = () => {
-	return <div>WhoIsAidie</div>;
+  return (
+    <div className="presentation-container">
+      <div className="top-section">
+        <div className="text-box pink-border">
+        <h1>Meet Aidie, your event assistant</h1>
+        <p>Aidie will help you to not miss your events</p>
+        </div>
+        <div className="character-image">
+          <img src={aidieLarge} alt="Aidie Large" className="aidie-large" />
+        </div>
+      </div>
+
+      <div className="middle-section">
+        <div className="text-box">
+          <img src={aidieSmall} alt="Aidie Small" className="aidie-small" />
+		  <h2>Pick your favorite artist</h2>
+        </div>
+        <div className="text-box">
+          <h3>Aidie will provide the next events in <br /> your area based on your preferents <img src="maps.png" alt="Icon" className="inline-image" /></h3>
+        </div>
+      </div>
+
+      {/* Parte inferior: 2 divs uno al lado del otro */}
+      <div className="bottom-section">
+        <div className="text-box">
+          <p>Choose your preferences and Aidie will handle the rest</p>
+        </div>
+        <div className="text-box">
+          <p>Get reminders for events, updates, and more</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WhoIsAidie;
